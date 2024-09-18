61769efece15ca266e8ea76677b277d18270fea1
Sana-7077 <121369686+Sana-7077@users.noreply.github.com>
2024-09-18 16:56:01 +0530

    cloning

---
+++ script.js
@@ -0,0 +1,8 @@
+function lang(){
+    document.getElementsByClassName("down-icon"){
+        <select>
+    <option lang="en" value="en-IN" selected>English</option>
+    <option lang="hi" value="hi-IN">हिन्दी</option>
+    </select>
+    }
+}
